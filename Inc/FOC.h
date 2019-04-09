cbe9c4faf464ece48765023087e17c65ab8460d2
stancecoke <hochsitzcola@web.de>
2019-04-09 11:25:13 +0200

    minimal ADC duration per #define

--- Inc/FOC.h
+++ Inc/FOC.h
@@ -9,7 +9,7 @@
 #define FOC_H_
 
 #include <arm_math.h>
-
+//exportetd functions
 void FOC_calculation(int16_t int16_i_as, int16_t int16_i_bs, q31_t q31_teta, int16_t int16_i_q_target);
 q31_t PI_control_i_q (q31_t ist, q31_t soll);
 q31_t PI_control_i_d (q31_t ist, q31_t soll);
@@ -18,13 +18,8 @@ q31_t PI_control_i_d (q31_t ist, q31_t soll);
 #define _U_MAX	2030L  //little lower than period of timer1 for proper phase current reading. Could be improved by dynamic timing of AD-conversion
 // Square Root of 3
 #define _SQRT3	1.73205081
-// Pi divided by 3
 
-
-
-
-
-
+#define ADC_DUR 350 //minimal duration for proper ADC reading deadtime + noise subsiding + sample time
 
 
 //globals
