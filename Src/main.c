3b9d0d0399a56748ead84c7d68d72f54278659ae
stancecoke <graf.koks@freenet.de>
2019-09-03 17:34:10 +0200

    v0.3 experimental switch of current reading channels

--- Src/main.c
+++ Src/main.c
@@ -400,7 +400,7 @@ int main(void)
    		 q31_rotorposition_absolute = q31_rotorposition_hall; // set absolute position to corresponding hall pattern.
 
 
-    printf_("Lishui FOC v0.2 \n ");
+    printf_("Lishui FOC v0.3 \n ");
     HAL_Delay(5);
 
 
@@ -1032,8 +1032,8 @@ void HAL_ADCEx_InjectedConvCpltCallback(ADC_HandleTypeDef* hadc)
 
 #ifdef DISABLE_DYNAMIC_ADC
 
-		i16_ph1_current = HAL_ADCEx_InjectedGetValue(&hadc1, ADC_INJECTED_RANK_1);
-		i16_ph2_current = HAL_ADCEx_InjectedGetValue(&hadc2, ADC_INJECTED_RANK_1);
+		i16_ph2_current = HAL_ADCEx_InjectedGetValue(&hadc1, ADC_INJECTED_RANK_1);
+		i16_ph1_current = HAL_ADCEx_InjectedGetValue(&hadc2, ADC_INJECTED_RANK_1);
 
 #else
 	switch (char_dyn_adc_state) //read in according to state
