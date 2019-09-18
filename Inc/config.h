bdd024f0caf48a590109d5e8f8bce27e73641b76
stancecoke <graf.koks@freenet.de>
2019-09-18 19:57:57 +0200

    parameter adjustment

--- Inc/config.h
+++ Inc/config.h
@@ -31,7 +31,7 @@
 
 
 #define SPEC_ANGLE  -715827882L		//motor specific angle, refer to chapter 8.3.3 of UM1052.shengyi:-715827882L, 715827882 536870912 357913941L; //357913941 298261617 119304647L // 30° BionX IGH3 motor specific angle, refer to chapter 8.8.3 of UM1052 180° maped to 2^31
-#define FILTER_DELAY 536870912L	// for angle correction of i_alfa + i_beta
+#define FILTER_DELAY 536870912	 //1073741824L	// for angle correction of i_alfa + i_beta
 
 #define OFFSET_A 1993 //1025 				//Offset of current sensing phase A
 #define OFFSET_B 1977 //1022				//Offset of current sensing phase B
@@ -44,7 +44,7 @@
 //Constants for Motor model of observer
 #define INDUCTANCE	6LL		//war nach Messung 13 (mit einfachem LCR-Tester)		//H = V*s/A Induktivität in µH/100 Shengyi hat 200µHenry Induktivität 2^16*0,0002 -->>>16 in Observer um auf Henry zukommen
 #define RESISTANCE 40LL		//war nach Messung 60	//Ohm = V/A Widerstand in Shengi ist 117mOhm -->2^9*0,117 >>9 in Observer um auf Ohm zu kommen.
-#define FLUX_LINKAGE 1200LL			//V*s/rad von Hand angepasst
+#define FLUX_LINKAGE 1600LL			//V*s/rad von Hand angepasst
 #define GAMMA 10LL					//per trial and error
 
 
