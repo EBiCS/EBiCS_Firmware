e94fb1868383edb0697284750d8a8581fe4b2054
stancecoke <graf.koks@freenet.de>
2019-09-22 18:35:53 +0200

    bugfix in diplays #defines

--- Inc/config.h
+++ Inc/config.h
@@ -6,8 +6,8 @@
 #define DISPLAY_TYPE_KINGMETER_618U (1<<4)                  // King-Meter 618U protocol (KM5s, EBS-LCD2, J-LCD, SW-LCD)
 #define DISPLAY_TYPE_KINGMETER_901U (1<<8)                  // King-Meter 901U protocol (KM5s)
 #define DISPLAY_TYPE_KINGMETER      (DISPLAY_TYPE_KINGMETER_618U|DISPLAY_TYPE_KINGMETER_901U)
-#define DISPLAY_TYPE_BAFANG (1>>2)							// For "Blaupunkt" Display of Prophete Entdecker
-#define DISPLAY_TYPE_DEBUG (1>>0)							// For ASCII-Output in Debug mode
+#define DISPLAY_TYPE_BAFANG (1<<2)							// For "Blaupunkt" Display of Prophete Entdecker
+#define DISPLAY_TYPE_DEBUG (1<<0)							// For ASCII-Output in Debug mode
 
 //#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_618U			//Kingmeter J-LCD
 //#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U			//Kingmeter KM5S
