b6dd2bb946b1bf741c4c16c9ee92500e8226ab2e
stancecoke <graf.koks@freenet.de>
2020-01-12 19:57:31 +0100

    flashing via STLink utility CLI added

--- Start_Compiling.bat
+++ Start_Compiling.bat
@@ -1,4 +1,4 @@
-PATH = %PATH%;C:\GNU_ARM\eclipse\plugins\fr.ac6.mcu.externaltools.arm-none.win32_1.16.0.201807130628\tools\make;C:\GNU_ARM\eclipse\plugins\fr.ac6.mcu.externaltools.arm-none.win32_1.16.0.201807130628\tools\compiler\bin
+PATH = %PATH%;C:\GNU_ARM\eclipse\plugins\fr.ac6.mcu.externaltools.arm-none.win32_1.16.0.201807130628\tools\make;C:\GNU_ARM\eclipse\plugins\fr.ac6.mcu.externaltools.arm-none.win32_1.16.0.201807130628\tools\compiler\bin;C:\Program Files (x86)\STMicroelectronics\STM32 ST-LINK Utility\ST-LINK Utility
 cd debug
 
 make clean
@@ -16,6 +16,8 @@ copy subdir_src.mk Src\subdir.mk
 copy subdir_startup.mk startup\subdir.mk
 
 make all
+
+ST-LINK_CLI.exe -c SWD -P LishuiFOC_01.hex -V
 
 pause
 
