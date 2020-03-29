bcb4248abd012ffc738deef1dddebca7742c174b
stancecoke <graf.koks@freenet.de>
2020-03-29 14:39:26 +0200

    Bugfix in speed for Bafang display

--- Src/main.c
+++ Src/main.c
@@ -1449,7 +1449,7 @@ void bafang_update(void)
     if(__HAL_TIM_GET_COUNTER(&htim2) < 12000)
     {
         // Adapt wheeltime to match displayed speedo value according config.h setting
-        BF.Tx.Wheeltime_ms = WHEEL_CIRCUMFERENCE*433/(MS.Speed*PULSES_PER_REVOLUTION); // Geschwindigkeit ist Weg pro Zeit Radumfang durch Dauer einer Radumdrehung --> Umfang * 16000*3600/(n*1000000) * Skalierung Bafang Display 200/26,6
+        BF.Tx.Wheeltime_ms = WHEEL_CIRCUMFERENCE*216/(MS.Speed*PULSES_PER_REVOLUTION); // Geschwindigkeit ist Weg pro Zeit Radumfang durch Dauer einer Radumdrehung --> Umfang * 8000*3600/(n*1000000) * Skalierung Bafang Display 200/26,6
 
     }
     else
