817d1780aa2635590d2e114cde257c8cd4ff6e6c
stancecoke <graf.koks@freenet.de>
2021-01-20 11:55:40 +0100

    regen only when speed > 6kph

--- Src/main.c
+++ Src/main.c
@@ -603,7 +603,7 @@ int main(void)
 
 
 				if(!HAL_GPIO_ReadPin(Brake_GPIO_Port, Brake_Pin)){
-					if(uint32_tics_filtered>>3<15000)int32_current_target=-REGEN_CURRENT; //only apply regen, if motor is turning fast enough
+					if(tics_to_speed(uint32_tics_filtered>>3)>6)int32_current_target=-REGEN_CURRENT; //only apply regen, if motor is turning fast enough
 					else int32_current_target=0;
 				}
 				//next priority: undervoltage protection
@@ -740,7 +740,7 @@ int main(void)
 		  //print values for debugging
 
 
-		  sprintf_(buffer, "%d, %d, %d, %d, %d, %d, %d\r\n", MS.i_q,int32_current_target, (int16_t)MS.Battery_Current, (uint16_t)adcData[1], (uint16_t)MS.u_abs,tics_to_speed(uint32_tics_filtered>>3) , (uint16_t)(adcData[6]));
+		  sprintf_(buffer, "%d, %d, %d, %d, %d, %d, %d\r\n", MS.i_q,int32_current_target, (int16_t)MS.Battery_Current, MS.i_d, (uint16_t)MS.u_abs,tics_to_speed(uint32_tics_filtered>>3) , (uint16_t)(adcData[6]));
 		 // sprintf_(buffer, "%d, %d, %d, %d, %d, %d\r\n",ui8_hall_state,(uint16_t)adcData[1],(uint16_t)adcData[2],(uint16_t)adcData[3],(uint16_t)(adcData[4]),(uint16_t)(adcData[5])) ;
 
 	  	  i=0;
