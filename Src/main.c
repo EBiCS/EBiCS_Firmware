ee7ad5d1c7e47162e65241698043b83fd7cc2af5
stancecoke <hochsitzcola@web.de>
2022-05-11 20:18:38 +0200

    Bugfix in external speed sensor processing for KM5S protocol

--- Src/main.c
+++ Src/main.c
@@ -1907,19 +1907,18 @@ void kingmeter_update(void)
         KM.Tx.Battery = KM_BATTERY_LOW;
     }
 
-    if(__HAL_TIM_GET_COUNTER(&htim2) < 12000)
-    {
+
         // Adapt wheeltime to match displayed speedo value according config.h setting
 #if (SPEEDSOURCE  == EXTERNAL)
     	KM.Tx.Wheeltime_ms = ((MS.Speed>>3)*PULSES_PER_REVOLUTION); //>>3 because of 8 kHz counter frequency, so 8 tics per ms
+
 #else
-    	KM.Tx.Wheeltime_ms = (MS.Speed*GEAR_RATIO*6)>>9; //>>9 because of 500kHZ timer2 frequency, 512 tics per ms should be OK *6 because of 6 hall interrupts per electric revolution.
-#endif
-    }
-    else
-    {
-        KM.Tx.Wheeltime_ms = 64000;
-    }
+        if(__HAL_TIM_GET_COUNTER(&htim2) < 12000)  	KM.Tx.Wheeltime_ms = (MS.Speed*GEAR_RATIO*6)>>9; //>>9 because of 500kHZ timer2 frequency, 512 tics per ms should be OK *6 because of 6 hall interrupts per electric revolution.
+        else  KM.Tx.Wheeltime_ms = 64000;
+
+        #endif
+
+
 
 
     //KM.Tx.Wheeltime_ms = 25;
