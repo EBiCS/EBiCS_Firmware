bb577c81d074470ac45a27dc80f0773f0ad61baa
stancecoke <hochsitzcola@web.de>
2024-04-16 07:51:00 +0200

    try to make compiling work by action

--- Src/subdir.mk
+++ Src/subdir.mk
@@ -4,61 +4,53 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-Src/FOC.c \
-Src/display_bafang.c \
-Src/display_kingmeter.c \
-Src/display_kunteng.c \
-Src/M365_Dashboard.c \
-Src/button_processing.c \
-Src/eeprom.c \
-Src/main.c \
-Src/print.c \
-Src/stm32f1xx_hal_msp.c \
-Src/stm32f1xx_it.c \
-Src/system_stm32f1xx.c 
+../Src/FOC.c \
+../Src/M365_Dashboard.c \
+../Src/button_processing.c \
+../Src/display_bafang.c \
+../Src/display_kingmeter.c \
+../Src/display_kunteng.c \
+../Src/eeprom.c \
+../Src/main.c \
+../Src/print.c \
+../Src/stm32f1xx_hal_msp.c \
+../Src/stm32f1xx_it.c \
+../Src/system_stm32f1xx.c 
 
 OBJS += \
-build/FOC.o \
-build/display_bafang.o \
-build/display_kingmeter.o \
-build/display_kunteng.o \
-Src/M365_Dashboard.o \
-Src/button_processing.o \
-build/eeprom.o \
-build/main.o \
-build/print.o \
-build/stm32f1xx_hal_msp.o \
-build/stm32f1xx_it.o \
-build/system_stm32f1xx.o 
+./Src/FOC.o \
+./Src/M365_Dashboard.o \
+./Src/button_processing.o \
+./Src/display_bafang.o \
+./Src/display_kingmeter.o \
+./Src/display_kunteng.o \
+./Src/eeprom.o \
+./Src/main.o \
+./Src/print.o \
+./Src/stm32f1xx_hal_msp.o \
+./Src/stm32f1xx_it.o \
+./Src/system_stm32f1xx.o 
 
 C_DEPS += \
-build/FOC.d \
-build/display_bafang.d \
-build/display_kingmeter.d \
-build/display_kunteng.d \
-Src/M365_Dashboard.d \
-Src/button_processing.d \
-build/eeprom.d \
-build/main.d \
-build/print.d \
-build/stm32f1xx_hal_msp.d \
-build/stm32f1xx_it.d \
-build/system_stm32f1xx.d 
-
-INC_PATH = Inc
-DRIVERS_PATH = Drivers/STM32F1xx_HAL_Driver/Inc
-LEGACY_PATH = Drivers/STM32F1xx_HAL_Driver/Inc/Legacy
-DEVICE_PATH = Drivers/CMSIS/Device/ST/STM32F1xx/Include
-CMSIS_PATH = Drivers/CMSIS/Include
+./Src/FOC.d \
+./Src/M365_Dashboard.d \
+./Src/button_processing.d \
+./Src/display_bafang.d \
+./Src/display_kingmeter.d \
+./Src/display_kunteng.d \
+./Src/eeprom.d \
+./Src/main.d \
+./Src/print.d \
+./Src/stm32f1xx_hal_msp.d \
+./Src/stm32f1xx_it.d \
+./Src/system_stm32f1xx.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-build/%.o: Src/%.c
+Src/%.o: ../Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' -DARM_MATH_CM3 '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xB -I $(INC_PATH) -I $(DRIVERS_PATH) -I $(LEGACY_PATH) -I $(DEVICE_PATH) -I $(CMSIS_PATH) -Os -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' -DARM_MATH_CM3 '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F103xB -I"C:/Users/gaswerke/git/EBiCS_Firmware/Inc" -I"C:/Users/gaswerke/git/EBiCS_Firmware/Drivers/STM32F1xx_HAL_Driver/Inc" -I"C:/Users/gaswerke/git/EBiCS_Firmware/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy" -I"C:/Users/gaswerke/git/EBiCS_Firmware/Drivers/CMSIS/Device/ST/STM32F1xx/Include" -I"C:/Users/gaswerke/git/EBiCS_Firmware/Drivers/CMSIS/Include" -I"C:/CMSIS_5-develop/CMSIS_5-develop/CMSIS//Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
-	@echo ' '
-
-
+	@echo '
