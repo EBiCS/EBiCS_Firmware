270a31148d407a42766f0ccf48bfe84c4f319999
stancecoke <31093785+stancecoke@users.noreply.github.com>
2024-04-16 21:08:53 +0200

    Update M365_Dashboard.c

--- Src/M365_Dashboard.c
+++ Src/M365_Dashboard.c
@@ -141,7 +141,7 @@ void process_DashboardMessage(MotorState_t *MS, MotorParams_t *MP, uint8_t *mess
 			else if(MS->error_state==brake)MS->error_state=none;
 			if(map(message[Brake],BRAKEOFFSET,BRAKEMAX,0,MP->regen_current)>0){
 				MS->brake_active=true;
-				if(1){//(MS->Speed>2){
+				if(MS->Speed>2){
 					MS->i_q_setpoint_temp =map(message[Brake],BRAKEOFFSET,BRAKEMAX,0,MP->regen_current);
 					// ramp down regen strength at the max voltage to avoid the BMS shutting down the battery.
 					MS->i_q_setpoint_temp =-map(MS->Voltage*CAL_BAT_V,BATTERYVOLTAGE_MAX-1000,BATTERYVOLTAGE_MAX,MS->i_q_setpoint_temp,0);
