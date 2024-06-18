04ecf2f46239d71dfa234e22d5e73e8038258cc2
viktoschi <12160746+viktoschi@users.noreply.github.com>
2024-06-18 14:49:38 +0200

    Update main.h on GPIO Pins for Light & Brake Light (#40)
    Should be GPIOB not GPIOA

--- Inc/main.h
+++ Inc/main.h
@@ -73,9 +73,9 @@
 #define LED_Pin GPIO_PIN_2
 #define LED_GPIO_Port GPIOB
 #define LIGHT_Pin GPIO_PIN_9
-#define LIGHT_GPIO_Port GPIOA
+#define LIGHT_GPIO_Port GPIOB
 #define BRAKE_LIGHT_Pin GPIO_PIN_10
-#define BRAKE_LIGHT_GPIO_Port GPIOA
+#define BRAKE_LIGHT_GPIO_Port GPIOB
 #define PAS_Pin GPIO_PIN_8
 #define PAS_GPIO_Port GPIOB
 #define Brake_Pin GPIO_PIN_15  // put a 15 here for new generation controllers!
