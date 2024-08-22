d6a2a07c1cd4eb0f31a53c635deb0d18c10382a0
Antoine Sauray <6672783+asauray@users.noreply.github.com>
2024-08-22 15:23:08 +0200

    Update speed limit to 40kmh

--- Inc/config.h
+++ Inc/config.h
@@ -10,14 +10,14 @@
 #include "stdint.h"
 
 // System constants, don't touch!
-#define DISPLAY_TYPE_DEBUG (1<<0)							// For ASCII-Output in Debug mode);
-#define DISPLAY_TYPE_KUNTENG (1<<1)							// For Kunteng display
-#define DISPLAY_TYPE_BAFANG (1<<2)							// For 'Blaupunkt' Display of Prophete Entdecker
-#define DISPLAY_TYPE_KINGMETER_618U (1<<3)                  // King-Meter 618U protocol ( J-LCD)
-#define DISPLAY_TYPE_KINGMETER_901U (1<<4)                  // King-Meter 901U protocol (KM5s)
-#define DISPLAY_TYPE_EBiCS (1<<5)                  			// Protocol using the ANT+ LEV logic
-#define DISPLAY_TYPE_NO2 (1<<6)								// For China Protokoll "No_2" S866 display for example
-#define DISPLAY_TYPE_KINGMETER      (DISPLAY_TYPE_KINGMETER_618U|DISPLAY_TYPE_KINGMETER_901U)
+#define DISPLAY_TYPE_DEBUG (1 << 0)          // For ASCII-Output in Debug mode);
+#define DISPLAY_TYPE_KUNTENG (1 << 1)        // For Kunteng display
+#define DISPLAY_TYPE_BAFANG (1 << 2)         // For 'Blaupunkt' Display of Prophete Entdecker
+#define DISPLAY_TYPE_KINGMETER_618U (1 << 3) // King-Meter 618U protocol ( J-LCD)
+#define DISPLAY_TYPE_KINGMETER_901U (1 << 4) // King-Meter 901U protocol (KM5s)
+#define DISPLAY_TYPE_EBiCS (1 << 5)          // Protocol using the ANT+ LEV logic
+#define DISPLAY_TYPE_NO2 (1 << 6)            // For China Protokoll "No_2" S866 display for example
+#define DISPLAY_TYPE_KINGMETER (DISPLAY_TYPE_KINGMETER_618U | DISPLAY_TYPE_KINGMETER_901U)
 #define EXTERNAL 1
 #define INTERNAL 0
 //----------------------------------------------------------------------
@@ -27,22 +27,22 @@
 #define _T 2028
 #define CAL_BAT_V 256
 #define CAL_V 25
-#define CAL_I 38LL<<8
+#define CAL_I 38LL << 8
 #define INDUCTANCE 6LL
 #define RESISTANCE 40LL
 #define FLUX_LINKAGE 1200LL
 #define GAMMA 9LL
-//#define FAST_LOOP_LOG
-//#define DISABLE_DYNAMIC_ADC
-//#define INDIVIDUAL_MODES
-//#define SPEEDTHROTTLE
+// #define FAST_LOOP_LOG
+// #define DISABLE_DYNAMIC_ADC
+// #define INDIVIDUAL_MODES
+// #define SPEEDTHROTTLE
 #define SIXSTEPTHRESHOLD 20000
-#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
+#define SPEED_PLL 0 // 1 for using PLL, 0 for angle extrapolation
 #define P_FACTOR_PLL 10
 #define I_FACTOR_PLL 10
 
 //----------------------------------------------------------------------
-//Battery bar settings for Kunteng and Bafang Display
+// Battery bar settings for Kunteng and Bafang Display
 #define BATTERY_LEVEL_1 323000
 #define BATTERY_LEVEL_2 329000
 #define BATTERY_LEVEL_3 344000
@@ -50,7 +50,7 @@
 #define BATTERY_LEVEL_5 380000
 
 //----------------------------------------------------------------------
-//PI-control factor settings
+// PI-control factor settings
 #define P_FACTOR_I_Q 50
 #define I_FACTOR_I_Q 2
 #define P_FACTOR_I_D 50
@@ -59,53 +59,52 @@
 #define I_FACTOR_SPEED 10
 
 //----------------------------------------------------------------------
-//PAS mode settings
-//#define DIRDET
+// PAS mode settings
+// #define DIRDET
 #define FRAC_HIGH 30
 #define FRAC_LOW 15
 #define PAS_TIMEOUT 3000
 #define RAMP_END 1200
 
 //---------------------------------------------------------------------
-//Throttle settings
-#define THROTTLE_OFFSET 1250   //only default value, throttle offset is set at startup automatically
+// Throttle settings
+#define THROTTLE_OFFSET 1250 // only default value, throttle offset is set at startup automatically
 #define THROTTLE_MAX 2850
 #define THROTTLE_OVERRIDE
 
 //--------------------------------------------------------------------
-//Speed settings
+// Speed settings
 #define WHEEL_CIRCUMFERENCE 2200
-#define GEAR_RATIO 11 //11 for BionX IGH3
-#define SPEEDLIMIT 25
+#define GEAR_RATIO 98 // 11 for BionX IGH3
+#define SPEEDLIMIT 40
 #define PULSES_PER_REVOLUTION 1
-#define SPEEDSOURCE INTERNAL
+#define SPEEDSOURCE EXTERNAL
 #define SPEEDFILTER 1
 #define SPDSHFT 0
 
 //---------------------------------------------------------------------
-//power settings
+// power settings
 #define PH_CURRENT_MAX 1200
 #define BATTERYCURRENT_MAX 14000
-#define REVERSE 1 //1 for normal direction, -1 for reverse
+#define REVERSE 1 // 1 for normal direction, -1 for reverse
 #define PUSHASSIST_CURRENT 300
-#define VOLTAGE_MIN 1320 //33V
-
+#define VOLTAGE_MIN 1320 // 33V
 //---------------------------------------------------------------------
-//torquesensor settings
+// torquesensor settings
 #define TS_COEF 2400
-#define TS_MODE
-//#define TQONAD1
+// #define TS_MODE
+// #define TQONAD1
 
 //---------------------------------------------------------------------
-//Display settings
+// Display settings
 #define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U
 
 //---------------------------------------------------------------------
-//Regen settings
+// Regen settings
 
 #define REGEN_CURRENT 800
 #define REGEN_CURRENT_MAX 10000
-//#define ADC_BRAKE
+// #define ADC_BRAKE
 
 //---------------------------------------------------------------------
 #define AUTODETECT 0
