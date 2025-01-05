61c4bdcdd5e1370dab04194b70d5f5d08fbec6fb
Antoine Sauray <6672783+asauray@users.noreply.github.com>
2025-01-05 21:51:10 +0100

    set display type to DISPLAY_TYPE_KINGMETER_901U

--- Inc/config.h
+++ Inc/config.h
@@ -97,7 +97,7 @@
 
 //---------------------------------------------------------------------
 //Display settings
-#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_618U 
+#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U 
 
 //---------------------------------------------------------------------
 //Regen settings
