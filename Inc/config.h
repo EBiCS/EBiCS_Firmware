fb59d56cce0019697a1d19cbb8ce3eb73176df59
Antoine Sauray <6672783+asauray@users.noreply.github.com>
2025-01-06 18:39:14 +0100

    Update config.h debug

--- Inc/config.h
+++ Inc/config.h
@@ -97,7 +97,7 @@
 
 //---------------------------------------------------------------------
 // Display settings
-#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U
+#define DISPLAY_TYPE DISPLAY_TYPE_DEBUG
 
 //---------------------------------------------------------------------
 // Regen settings
@@ -107,6 +107,6 @@
 // #define ADC_BRAKE
 
 //---------------------------------------------------------------------
-#define AUTODETECT 0
+#define AUTODETECT 1
 
 #endif /* CONFIG_H_ */
