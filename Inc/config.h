7b4cc84781356243aeb0bbe5de96a72138fcef9f
Antoine Sauray <6672783+asauray@users.noreply.github.com>
2025-01-06 19:21:44 +0100

    use kingmeter

--- Inc/config.h
+++ Inc/config.h
@@ -97,7 +97,7 @@
 
 //---------------------------------------------------------------------
 // Display settings
-#define DISPLAY_TYPE DISPLAY_TYPE_DEBUG
+#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER
 
 //---------------------------------------------------------------------
 // Regen settings
