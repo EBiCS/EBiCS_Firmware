b15db946a9716713deb09fa8e08a43c899d0adca
tmko13 <97597352+tmko13@users.noreply.github.com>
2025-02-19 23:14:05 +0100

    add variable for Number of Impulses per PAS turn

--- Src/main.c
+++ Src/main.c
@@ -718,7 +718,7 @@ int main(void)
 				uint32_PAS_counter =0;
 				ui8_PAS_flag=0;
 				//read in and sum up torque-signal within one crank revolution (for sempu sensor 32 PAS pulses/revolution, 2^5=32)
-				uint32_torque_cumulated -= uint32_torque_cumulated>>5;
+				uint32_torque_cumulated -= uint32_torque_cumulated/PAS_IMP_PER_TURN;
 #ifdef NCTE
 				if(ui16_throttle<ui16_throttle_offset)uint32_torque_cumulated += (ui16_throttle_offset-ui16_throttle);
 #else
@@ -817,7 +817,7 @@ int main(void)
 
 #ifdef TS_MODE //torque-sensor mode
 				//calculate current target form torque, cadence and assist level
-				int32_temp_current_target = (TS_COEF*(int32_t)(MS.assist_level)* (uint32_torque_cumulated>>5)/uint32_PAS)>>8; //>>5 aus Mittelung über eine Kurbelumdrehung, >>8 aus KM5S-Protokoll Assistlevel 0..255
+				int32_temp_current_target = (TS_COEF*(int32_t)(MS.assist_level)* (uint32_torque_cumulated/PAS_IMP_PER_TURN)/uint32_PAS)>>8; // >>8 aus KM5S-Protokoll Assistlevel 0..255
 
 				//limit currest target to max value
 				if(int32_temp_current_target>PH_CURRENT_MAX) int32_temp_current_target = PH_CURRENT_MAX;
