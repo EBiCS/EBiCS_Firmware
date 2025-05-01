1c91455550851bd390a05fd57e5fa019de7571ea
tecnologic <tecnologic86@gmail.com>
2025-05-01 17:53:32 +0200

    Current running config

--- Inc/config.h
+++ Inc/config.h
@@ -8,8 +8,6 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 #include "stdint.h"
-
-// System constants, don't touch!
 #define DISPLAY_TYPE_DEBUG (1<<0)							// For ASCII-Output in Debug mode);
 #define DISPLAY_TYPE_KUNTENG (1<<1)							// For Kunteng display
 #define DISPLAY_TYPE_BAFANG_LCD (1<<2)						// For 'Blaupunkt' Display of Prophete Entdecker
@@ -22,95 +20,61 @@
 #define DISPLAY_TYPE_KINGMETER      (DISPLAY_TYPE_KINGMETER_618U|DISPLAY_TYPE_KINGMETER_901U)
 #define EXTERNAL 1
 #define INTERNAL 0
-//----------------------------------------------------------------------
-// advanced setting, don't touch, if you don't know what you are doing!
+#define LEGALFLAG 
+
 #define TRIGGER_OFFSET_ADC 50
 #define TRIGGER_DEFAULT 2020
 #define _T 2028
 #define CAL_BAT_V 256
 #define CAL_V 25
 #define CAL_I 38LL<<8
-#define INDUCTANCE 6LL
-#define RESISTANCE 40LL
-#define FLUX_LINKAGE 1200LL
-#define GAMMA 9LL
-//#define FAST_LOOP_LOG
-//#define DISABLE_DYNAMIC_ADC
-//#define INDIVIDUAL_MODES
-//#define SPEEDTHROTTLE
-#define SIXSTEPTHRESHOLD 20000
-#define SPEED_PLL 0 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 10
-#define I_FACTOR_PLL 10
-
-//----------------------------------------------------------------------
-//Battery bar settings for Kunteng and Bafang Display
-#define BATTERY_LEVEL_1 323000
-#define BATTERY_LEVEL_2 329000
-#define BATTERY_LEVEL_3 344000
-#define BATTERY_LEVEL_4 368000
-#define BATTERY_LEVEL_5 380000
-
-//----------------------------------------------------------------------
-//PI-control factor settings
-#define P_FACTOR_I_Q 50
-#define I_FACTOR_I_Q 2
-#define P_FACTOR_I_D 50
-#define I_FACTOR_I_D 2
-#define P_FACTOR_SPEED 1
+#define INDUCTANCE 6
+#define RESISTANCE 40
+#define FLUX_LINKAGE 1200
+#define GAMMA 9
+#define BATTERY_LEVEL_1 430000
+#define BATTERY_LEVEL_2 450000
+#define BATTERY_LEVEL_3 470000
+#define BATTERY_LEVEL_4 490000
+#define BATTERY_LEVEL_5 510000
+#define P_FACTOR_I_Q 500
+#define I_FACTOR_I_Q 20
+#define P_FACTOR_I_D 500
+#define I_FACTOR_I_D 20
+#define P_FACTOR_PLL 5
+#define I_FACTOR_PLL 11
+#define P_FACTOR_SPEED 100
 #define I_FACTOR_SPEED 10
-
-//----------------------------------------------------------------------
-//PAS mode settings
-//#define DIRDET
+#define SPDSHFT 0
+#define SPEEDFILTER 1
+#define SIXSTEPTHRESHOLD 3500
+#define TS_COEF 60000
+#define PAS_TIMEOUT 8000
+#define RAMP_END 1600
+#define PAS_IMP_PER_TURN 32
 #define FRAC_HIGH 30
 #define FRAC_LOW 15
-#define PAS_TIMEOUT 3000
-#define RAMP_END 1200
-#define PAS_IMP_PER_TURN 32
-
-//---------------------------------------------------------------------
-//Throttle settings
-#define THROTTLE_OFFSET 1250   //only default value, throttle offset is set at startup automatically
-#define THROTTLE_MAX 2850
-#define THROTTLE_OVERRIDE
-
-//--------------------------------------------------------------------
-//Speed settings
+#define THROTTLE_OFFSET 750
+#define THROTTLE_MAX 2600
+#define PUSHASSIST_CURRENT 3000
 #define WHEEL_CIRCUMFERENCE 2200
-#define GEAR_RATIO 11 //11 for BionX IGH3
+#define GEAR_RATIO 88
 #define SPEEDLIMIT 25
-#define PULSES_PER_REVOLUTION 1
-#define SPEEDSOURCE INTERNAL
-#define SPEEDFILTER 1
-#define SPDSHFT 0
-
-//---------------------------------------------------------------------
-//power settings
-#define PH_CURRENT_MAX 1200
-#define BATTERYCURRENT_MAX 14000
-#define REVERSE 1 //1 for normal direction, -1 for reverse
-#define PUSHASSIST_CURRENT 300
-#define VOLTAGE_MIN 1320 //33V
-
-//---------------------------------------------------------------------
-//torquesensor settings
-#define TS_COEF 2400
-#define TS_MODE
-//#define TQONAD1
-
-//---------------------------------------------------------------------
-//Display settings
-#define DISPLAY_TYPE DISPLAY_TYPE_KINGMETER_901U
-
-//---------------------------------------------------------------------
-//Regen settings
-
-#define REGEN_CURRENT 800
+#define PULSES_PER_REVOLUTION 6
+#define PH_CURRENT_MAX 600
+#define BATTERYCURRENT_MAX 15000
+#define REGEN_CURRENT 200
 #define REGEN_CURRENT_MAX 10000
-//#define ADC_BRAKE
-
-//---------------------------------------------------------------------
+#define VOLTAGE_MIN 1600
+#define VOLTAGE_MAX 2200
+#define SPEC_ANGLE -715827882
+#define TS_MODE
+#define TQONAD1
+#define DISPLAY_TYPE DISPLAY_TYPE_KUNTENG //Kunteng LCD3/5 etc.
+#define SPEED_PLL 1
+#define SPEEDSOURCE EXTERNAL
 #define AUTODETECT 0
+#define REVERSE 1
+#define LIMIT_THR
 
 #endif /* CONFIG_H_ */
