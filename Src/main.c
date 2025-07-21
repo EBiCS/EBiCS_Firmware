3ea27a2a5b249d1b7c131d4c187e3fa3410a6555
Antoine Sauray <sauray.antoine@icloud.com>
2025-07-21 15:22:46 +0200

    pas de gachette si bride

--- Src/main.c
+++ Src/main.c
@@ -882,7 +882,10 @@ int main(void)
 
 #else //else NTCE
 				// read in throttle for throttle override
-				uint16_mapped_throttle = map(ui16_throttle, ui16_throttle_offset, THROTTLE_MAX, 0,PH_CURRENT_MAX);
+                // if not briddled, use throttle override
+                if(KM.Rx.SPEEDMAX_Limit == SPEEDLIMIT) {
+				    uint16_mapped_throttle = map(ui16_throttle, ui16_throttle_offset, THROTTLE_MAX, 0,PH_CURRENT_MAX);
+                }
 
 #endif //end NTCE
 
